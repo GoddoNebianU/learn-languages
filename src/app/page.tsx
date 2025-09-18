dc0ffce3c04d49056efc7d9dbfdf4e7f8b8e04ab
goddonebianu <goddonebianu@outlook.com>
2025-09-18 10:45:37 +0800

    2025-09-18-10-45

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,12 @@ function Link(
 
 export default function Home() {
   return (
-    <div className="w-[500px] m-auto mt-[100px] h-[300px]">
+    <div className="w-80 m-auto mt-[100px]">
+      <h1 className="mb-8 text-4xl font-bold">学外语</h1>
       <Link href="/srt-player" label="srt-player"></Link>
       <Link href="/word-board" label="word-board"></Link>
+      <p className="mt-8">srt-player: 一个基于srt字幕文件的逐句视频播放器，需要上传视频文件与字幕文件使用。</p>
+      <p>word-board: 一个板式单词记忆工具。</p>
     </div>
   );
 }
