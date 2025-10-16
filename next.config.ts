986be675b243c8eaedda1482107620db57b6a165
goddonebianu <goddonebianu@outlook.com>
2025-10-16 17:24:47 +0800

    add .drone.yml

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  allowedDevOrigins: ["192.168.3.65"]
 };
 
 export default nextConfig;
