5d2ec4ac5c96c689fa91be41937be5322f9b9671
goddonebianu <goddonebianu@outlook.com>
2025-11-16 20:50:31 +0800

    clean code

--- src/config/word-board-config.ts
+++
@@ -1,4 +0,0 @@
-export const BOARD_WIDTH = globalThis.innerWidth * 0.68;
-export const BOARD_HEIGHT = globalThis.innerHeight * 0.68;
-export const TEXT_SIZE = 30;
-export const TEXT_WIDTH = TEXT_SIZE * 0.6;
