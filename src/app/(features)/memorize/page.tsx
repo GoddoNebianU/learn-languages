f3b7f864132a1d3ce3275ad7359a7f0401521798
goddonebianu <goddonebianu@outlook.com>
2026-01-04 20:06:57 +0800

    取消了memorize folder界面的身份验证

--- src/app/(features)/memorize/page.tsx
+++ src/app/(features)/memorize/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 import { getTranslations } from "next-intl/server";
 import {
   getFoldersWithTotalPairsByUserId,
-  getUserIdByFolderId,
 } from "@/lib/server/services/folderService";
 import { isNonNegativeInteger } from "@/lib/utils";
 import FolderSelector from "./FolderSelector";
@@ -16,17 +15,7 @@ export default async function MemorizePage({
 }: {
   searchParams: Promise<{ folder_id?: string; }>;
 }) {
-  const session = await auth.api.getSession({ headers: await headers() });
   const tParam = (await searchParams).folder_id;
-
-  if (!session) {
-    redirect(
-      `/auth?redirect=/memorize${(await searchParams).folder_id
-        ? `?folder_id=${tParam}`
-        : ""
-      }`,
-    );
-  }
 
   const t = await getTranslations("memorize.page");
 
@@ -37,16 +26,13 @@ export default async function MemorizePage({
     : null;
 
   if (!folder_id) {
+    const session = await auth.api.getSession({ headers: await headers() });
+    if(!session) redirect("/auth?redirect=/memorize")
     return (
       <FolderSelector
         folders={await getFoldersWithTotalPairsByUserId(session.user.id)}
       />
     );
-  }
-
-  const owner = await getUserIdByFolderId(folder_id);
-  if (owner !== session.user.id) {
-    return <p>{t("unauthorized")}</p>;
   }
 
   return <Memorize textPairs={await getPairsByFolderId(folder_id)} />;
