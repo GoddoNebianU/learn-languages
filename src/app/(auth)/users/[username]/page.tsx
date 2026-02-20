6ea8b4d4b994144b1ed17f6bb44c4778fd146b0e
goddonebianu <goddonebianu@outlook.com>
2026-02-20 22:32:31 +0800

    ...

--- src/app/(auth)/users/[username]/page.tsx
+++ src/app/(auth)/users/[username]/page.tsx
@@ -42,7 +42,7 @@ export default async function UserPage({ params }: UserPageProps) {
             <div className="bg-white rounded-lg shadow-md p-6 mb-6">
                     <div className="flex items-center justify-between mb-4">
                         <div></div>
-                        {isOwnProfile && <LightButton>登出</LightButton>}
+                        {isOwnProfile && <LinkButton href="/logout">登出</LinkButton>}
                     </div>
                     <div className="flex items-center space-x-6">
                         {/* Avatar */}
